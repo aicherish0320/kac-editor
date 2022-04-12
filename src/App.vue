9e0db6941013ac3dcc4cd95114826dd6dd68c09a
aicherish0320 <aicherish0320@163.com>
2022-04-12 21:34:32 +0800

    init upload component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template>
+  <uploader action="http://localhost:3300/api/upload"></uploader>
   <router-view />
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Uploader from './components/Uploader.vue'
+export default defineComponent({
+  components: {
+    Uploader
+  }
+})
+</script>
 
 <style lang="scss"></style>
