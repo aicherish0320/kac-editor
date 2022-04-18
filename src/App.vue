5cae2aa3330cd880c592ec3b79eb293d95b12c0e
aicherish0320 <aicherish0320@163.com>
2022-04-18 15:51:03 +0800

    StyledUploader

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,14 @@
 <template>
-  <uploader
+  <!-- <uploader
     action="http://localhost:3300/api/upload"
     listType="picture"
-  ></uploader>
+  ></uploader> -->
   <router-view />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import Uploader from './components/Uploader.vue'
-export default defineComponent({
-  components: {
-    Uploader
-  }
-})
+export default defineComponent({})
 </script>
 
 <style lang="scss"></style>
