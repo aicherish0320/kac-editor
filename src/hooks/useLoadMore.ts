ade429e2788c3f75d6a43a6de0f6833ea65d79d9
aicherish0320 <aicherish0320@163.com>
2022-06-08 18:59:16 +0800

    useLoadMore

---
+++ src/hooks/useLoadMore.ts
@@ -0,0 +1,38 @@
+import { computed, ComputedRef, ref } from 'vue'
+import { useStore } from 'vuex'
+
+interface LoadParams {
+  pageIndex: number
+  pageSize: number
+  [key: string]: any
+}
+
+const useLoadMore = (
+  actionName: string,
+  total: ComputedRef<number>,
+  params: LoadParams = { pageIndex: 0, pageSize: 4 }
+) => {
+  const store = useStore()
+
+  const pageIndex = ref(params.pageIndex)
+
+  const requestParams = computed(() => {
+    return {
+      ...params,
+      pageIndex: pageIndex.value + 1
+    }
+  })
+
+  const loadMorePage = () => {
+    store
+      .dispatch(actionName, { searchParams: requestParams.value })
+      .then(() => pageIndex.value++)
+  }
+  const isLastPage = computed(() => {
+    // pageIndex 从 0 开始
+    return Math.ceil(total.value / params.pageSize) === pageIndex.value + 1
+  })
+  return { loadMorePage, isLastPage, pageIndex }
+}
+
+export default useLoadMore
